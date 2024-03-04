05e43bc4fcb6ffd35b6422e402ccacb25e19f822
Silentamv <159916683+Silentamv@users.noreply.github.com>
2024-03-04 21:13:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,10 +35,10 @@ API_HASH = environ.get("API_HASH", "03bbc5e15dbddfb03fb953435b5eb028")
 ADMINS = int(environ.get("ADMINS", "2056329003"))
 
 # Database Information
-CLONE_DB_URI = environ.get("CLONE_DB_URI", "")
-CDB_NAME = environ.get("CDB_NAME", "clonevjbotz")
-DB_URI = environ.get("DB_URI", "mongodb+srv://Silent:Arunava@123@atlascluster.btk4mu1.mongodb.net/?retryWrites=true&w=majority&appName=AtlasCluster")
-DB_NAME = environ.get("DB_NAME", "silentbot")
+CLONE_DB_URI = environ.get("CLONE_DB_URI", "mongodb+srv://Aru:VXLPtm8IhOMMklOR@atlascluster.btk4mu1.mongodb.net/?retryWrites=true&w=majority&appName=AtlasCluster")
+CDB_NAME = environ.get("CDB_NAME", "Aru")
+DB_URI = environ.get("mongodb+srv://Aru:VXLPtm8IhOMMklOR@atlascluster.btk4mu1.mongodb.net/?retryWrites=true&w=majority&appName=AtlasCluster")
+DB_NAME = environ.get("DB_NAME", "Aru")
 
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
