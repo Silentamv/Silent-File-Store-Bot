ccc8aa96c116ce0ddc5a5eae659a47c535e47861
Silentamv <159916683+Silentamv@users.noreply.github.com>
2024-03-07 13:05:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -95,9 +95,9 @@ class Var(object):
     FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
     HAS_SSL=bool(getenv('HAS_SSL',False))
     if HAS_SSL:
-        URL = ""
+        URL = "https://silent-bot-silentfilestorebot.koyeb.app/"
     else:
-        URL = ""
+        URL = "https://silent-bot-silentfilestorebot.koyeb.app/"
 
 
 
