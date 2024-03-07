2afed787d116b014186c2d2008a8416e82452dec
Silentamv <159916683+Silentamv@users.noreply.github.com>
2024-03-07 12:42:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,9 +35,9 @@ API_HASH = environ.get("API_HASH", "03bbc5e15dbddfb03fb953435b5eb028")
 ADMINS = int(environ.get("ADMINS", "2056329003"))
 
 # Database Information
-CLONE_DB_URI = environ.get("CLONE_DB_URI", "mongodb+srv://Aru:VXLPtm8IhOMMklOR@atlascluster.btk4mu1.mongodb.net/?retryWrites=true&w=majority&appName=AtlasCluster")
-CDB_NAME = environ.get("CDB_NAME", "Aru")
-DB_URI = environ.get("mongodb+srv://Aru:VXLPtm8IhOMMklOR@atlascluster.btk4mu1.mongodb.net/?retryWrites=true&w=majority&appName=AtlasCluster")
+CLONE_DB_URI = environ.get("CLONE_DB_URI", "mongodb+srv://Silent:WrducqXNpb7qkDOQ@silent.if8olkt.mongodb.net/?retryWrites=true&w=majority&appName=Silent")
+CDB_NAME = environ.get("CDB_NAME", "Silent")
+DB_URI = environ.get("mongodb+srv://Aru:xitheIzcM2MVw9Lf@aru.brkfhe9.mongodb.net/?retryWrites=true&w=majority&appName=Aru")
 DB_NAME = environ.get("DB_NAME", "Aru")
 
 # Don't Remove Credit Tg - @VJ_Botz
@@ -45,16 +45,17 @@ DB_NAME = environ.get("DB_NAME", "Aru")
 # Ask Doubt on telegram @KingVJ01
 
 # Bot Information
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
-BOT_USERNAME = environ.get("BOT_USERNAME", "@silentfilestorebot") # your bot username without @
-PICS = (environ.get('PICS', 'https://graph.org/file/82ef767ffebe3a948e476.jpg https://graph.org/file/82ef767ffebe3a948e476.jpg')).split() # Bot Start Picture
+BOT_TOKEN = environ.get("BOT_TOKEN", "7028356497:AAGzzr5J-4_3PfxFcbSAxlY71XTHtYzvncM
+")
+BOT_USERNAME = environ.get("BOT_USERNAME", "silentfilestorebot") # your bot username without @
+PICS = (environ.get('PICS', 'https://graph.org/file/51c6e0783b18c736fb8d7.jpg')).split() # Bot Start Picture
 
 # Auto Delete Information
-AUTO_DELETE = int(environ.get("AUTO_DELETE", "30")) # Time in Minutes
-AUTO_DELETE_TIME = int(environ.get("AUTO_DELETE_TIME", "1800")) # Time in Seconds
+AUTO_DELETE = int(environ.get("AUTO_DELETE", "10")) # Time in Minutes
+AUTO_DELETE_TIME = int(environ.get("AUTO_DELETE_TIME", "600")) # Time in Seconds
 
 # Channel Information
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002084706768"))
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002132361598"))
 FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '-1002007510451')).split()]
 
 # Don't Remove Credit Tg - @VJ_Botz
@@ -79,7 +80,7 @@ class Var(object):
     name = str(getenv('name', 'filetolinkvjbot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', ' -1002084706768'))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1002132361598'))
     PORT = int(getenv('PORT', 8080))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
@@ -94,7 +95,7 @@ class Var(object):
     FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
     HAS_SSL=bool(getenv('HAS_SSL',False))
     if HAS_SSL:
-        URL = "https://pleased-terry-silentfilestorebot.koyeb.app/"
+        URL = ""
     else:
         URL = ""
 
